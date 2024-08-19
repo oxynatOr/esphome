e6438b3e5b2335640b5d3b35e2592564e1854e9b
oxynatOr <98734567+oxynatOr@users.noreply.github.com>
2024-08-19 13:47:14 +0200

    try:-

--- esphome/components/pytes_e_box/sensor/__init__.py
+++ esphome/components/pytes_e_box/sensor/__init__.py
@@ -258,8 +258,8 @@ async def to_code(config):
 
     if CONF_CELL_ARRAYS in config:
         for cell_config in config[CONF_CELL_ARRAYS]:
-            cell_sensor = await cg.get_variable(cell_config[CONF_ID])
-            cell = cg.new_Pvariable(config[CONF_ID], config[CONF_BATTERY], cell_sensor) 
+            #cell_sensor = await cg.get_variable(cell_config[CONF_ID])
+            cell = cg.new_Pvariable(config[CONF_ID], config[CONF_BATTERY], cell_config[CONF_CELL]) 
             for marker in CELL_TYPES.items():
                 if marker_config := cell_config.get(marker):
                     sensor_var = await sensor.new_sensor(marker_config)

f767f866ff24c181319935ae5347ea2c9d3a3b58
oxynatOr <98734567+oxynatOr@users.noreply.github.com>
2024-08-19 14:10:04 +0200

    try:--

--- esphome/components/pytes_e_box/sensor/pytes_e_box_battery_cell_sensor.cpp
+++ esphome/components/pytes_e_box/sensor/pytes_e_box_battery_cell_sensor.cpp
@@ -8,11 +8,11 @@ namespace pytes_e_box {
 //static const char *const TAG_BS = "PytesEBoxbattery.sensor";
 
 
-PytesEBoxBatteryCellSensor::PytesEBoxBatteryCellSensor(int8_t bat_num, int8_t cell_num) { this->bat_num_ = bat_num; this->cell_num_ = cell_num; }
+PytesEBoxBatteryCellSensor::PytesEBoxBatteryCellSensor(int8_t cell_num) { this->cell_num_ = cell_num; }
 void PytesEBoxBatteryCellSensor::dump_config() {
   ESP_LOGCONFIG(TAG, "PytesEBox Battery Cell Sensor:");
-  ESP_LOGCONFIG(TAG, " Battery %d", this->bat_num_);
-  ESP_LOGCONFIG(TAG, " Battery %d", this->cell_num_);
+  //ESP_LOGCONFIG(TAG, " Battery %d", this->bat_num_);
+  ESP_LOGCONFIG(TAG, " Cell %d", this->cell_num_);
   LOG_SENSOR("  ","Voltage",this->cell_voltage_sensor_);
   LOG_SENSOR("  ","Current",this->cell_current_sensor_);
   LOG_SENSOR("  ","Temperature",this->cell_temperature_sensor_);
@@ -20,9 +20,9 @@ void PytesEBoxBatteryCellSensor::dump_config() {
 }
 
 void PytesEBoxBatteryCellSensor::on_batn_line_read(bat_index_LineContents *line) { return; 
-  if (this->bat_num_ != line->bat_num) {
-    return;    
-  }
+  // if (this->bat_num_ != line->bat_num) {
+  //   return;    
+  // }
   if (this->cell_num_ != line->bat_num) {
     return;    
   }  

7c45a6e34884243b97c210407f6b95535e950dc1
oxynatOr <98734567+oxynatOr@users.noreply.github.com>
2024-08-20 10:14:42 +0200

    fix: Import text_sensor

--- esphome/components/pytes_e_box/text_sensor/__init__.py
+++ esphome/components/pytes_e_box/text_sensor/__init__.py
@@ -1,7 +1,7 @@
 #import logging
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import text_sensor
 from esphome.const import (
     CONF_VOLTAGE,
     CONF_CURRENT,
